42ac1aa2d02281b3dd0a98f70ea51e1b4c158f72
Naana Shifah <shifahnana2@gmail.com>
2025-07-17 00:22:03 +0300

    Typing your functional components

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,9 @@
 export interface PillProps {
   title: string;
 }
+
+// interfaces/index.ts
+export interface ButtonProps {
+  title: string;
+  className?: string;
+}
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,4 +1,5 @@
 import Card from "@/components/card";
+import Button from "@/components/Button";
 import React from "react";
 
 const Landing: React.FC = () => {
@@ -6,6 +7,21 @@ const Landing: React.FC = () => {
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
+      
+      {/* Small Buttons */}
+      <Button title="Small - sm" className="text-sm rounded-sm" />
+      <Button title="Small - md" className="text-sm rounded-md" />
+      <Button title="Small - full" className="text-sm rounded-full" />
+
+      {/* Medium Buttons */}
+      <Button title="Medium - sm" className="text-base rounded-sm" />
+      <Button title="Medium - md" className="text-base rounded-md" />
+      <Button title="Medium - full" className="text-base rounded-full" />
+
+      {/* Large Buttons */}
+      <Button title="Large - sm" className="text-lg rounded-sm" />
+      <Button title="Large - md" className="text-lg rounded-md" />
+      <Button title="Large - full" className="text-lg rounded-full" />
     </div>
   );
 };
